56ee3dc7f035d55f0c3c8411861243781260a4da
Christopher Kelvin <christopherkelvin100@gmail.com>
2024-12-08 22:43:27 +0300

    get posts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 @Controller('users')
 export class UserController {
   @Get('/:id')
-  public getUsers(@Param() params: any) {
-    console.log(params);
+  public getUsers(@Param() params: any, @Query() query: any) {
+    console.log(params, query);
     return 'All users will be displayed here';
   }
   @Post()
-  public createUser() {
+  public createUser(@Body() request: any) {
+    console.log(request);
     return 'User created successfully';
   }
 }
