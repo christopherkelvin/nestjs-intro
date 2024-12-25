07326d7e544643361ee69578a54fdb9161c50401
Christopher Kelvin <christopherkelvin100@gmail.com>
2024-12-25 14:46:45 +0300

    Creating documentation with swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,14 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('NestJS API')
+    .setDescription('API for a simple NestJS application')
+    .addServer('http://localhost:3000')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
